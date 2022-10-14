78c6e45f695ee73008c0dc1624bec3d7924c88ed
hjs6877 <hjs6877@gmail.com>
2022-10-14 14:24:01 +0900

    Add RestTemplateBuilder Bean

--- src/main/java/com/codestates/Section4Week4TemplateWebfluxApplication.java
+++ src/main/java/com/codestates/Section4Week4TemplateWebfluxApplication.java
@@ -2,6 +2,8 @@ package com.codestates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Section4Week4TemplateWebfluxApplication {
@@ -10,4 +12,9 @@ public class Section4Week4TemplateWebfluxApplication {
 		SpringApplication.run(Section4Week4TemplateWebfluxApplication.class, args);
 	}
 
+
+	@Bean
+	public RestTemplateBuilder restTemplateBuilder() {
+		return new RestTemplateBuilder();
+	}
 }
